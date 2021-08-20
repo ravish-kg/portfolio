dfbd9f1a47bd2e342234750ada06f384e1ad4034
Ravish K G <ravishkg@Ravishs-Air.lan>
2021-08-20 16:07:44 -0700

    removed docs folder

--- src/App.js
+++ src/App.js
@@ -1,19 +1,19 @@
 import './App.css';
 import MainPage from './modules/MainPage';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import { BrowserRouter, HashRouter, Route, Switch } from 'react-router-dom';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import Login from './modules/Auth/Login';
 import CreateAccount from './modules/Auth/CreateAccount';
 
 function App() {
   return (
-    <HashRouter>
+    <BrowserRouter>
       <Switch>
         <Route path="/login" component={Login} />
         <Route path="/createAccount" component={CreateAccount} />
         <Route path="/" component={MainPage} />
       </Switch>
-    </HashRouter>
+    </BrowserRouter>
 
   );
 }
